ad0d26d90d5628ad5b0efd5c199d65323658a426
ShaikMahaboobbasha2005 <smehaboobbasha2005@gmail.com>
2026-07-06 20:33:30 +0530

    refactored the script

--- script.js
+++ script.js
@@ -1,60 +1,57 @@
-const p1Display = document.querySelector('#p1Display')
-const p2Display = document.querySelector('#p2Display')
-const p1Btn = document.querySelector('#p1Button')
-const p2Btn = document.querySelector('#p2Button')
-const resetBtn = document.querySelector('#reset')
-const winningScoreSelect = document.querySelector('#playto')
+const p1 = {
+    score: 0,
+    button: document.querySelector('#p1Button'),
+    display: document.querySelector('#p1Display')
+};
 
+const p2 = {
+    score: 0,
+    button: document.querySelector('#p2Button'),
+    display: document.querySelector('#p2Display')
+};
 
+const resetButton = document.querySelector('#reset');
+const winningScoreSelect = document.querySelector('#playto');
 
-let p1Score = 0
-let p2Score = 0
-let isGameover = false
-let winningScore = 5
+let winningScore = parseInt(winningScoreSelect.value);
+let isGameOver = false;
 
-p1Btn.addEventListener('click',function (){
-    if(!isGameover){
-        p1Score += 1
-        if(p1Score === winningScore){
-            isGameover = true
-            p1Display.classList.add('winner')
-            p2Display.classList.add('loser')
+function updateScores(player, opponent) {
+    if (!isGameOver) {
+        player.score++;
+
+        if (player.score === winningScore) {
+            isGameOver = true;
+            player.display.classList.add('winner');
+            opponent.display.classList.add('loser');
         }
-        p1Display.textContent = p1Score
+
+        player.display.textContent = player.score;
     }
-})
-
-
-p2Btn.addEventListener('click',function (){
-    if(!isGameover){
-        p2Score += 1
-        if(p2Score === winningScore){
-            isGameover = true
-            p2Display.classList.add('winner')
-            p1Display.classList.add('loser')
-        }
-        p2Display.textContent = p2Score
-    }
-})
-
-winningScoreSelect.addEventListener('change',function() {
-     winningScore = parseInt(this.value)
-    reset()
-})
-
-const reset = () =>{
-    isGameover = false
-    p1Score = 0
-    p2Score = 0
-    p1Display.textContent = 0
-    p2Display.textContent = 0
-    p1Display.classList.remove('winner','loser')
-    p2Display.classList.remove('loser','winner')
-
 }
-resetBtn.addEventListener('click',reset)
 
+p1.button.addEventListener('click', function () {
+    updateScores(p1, p2);
+});
 
+p2.button.addEventListener('click', function () {
+    updateScores(p2, p1);
+});
 
+winningScoreSelect.addEventListener('change', function () {
+    winningScore = parseInt(this.value);
+    reset();
+});
 
+resetButton.addEventListener('click', reset);
 
+function reset() {
+    isGameOver = false;
+    p1.score = 0;
+    p2.score = 0;
+
+    for (let p of [p1, p2]) {
+        p.display.textContent = 0;
+        p.display.classList.remove('winner', 'loser');
+    }
+}
